b10c15008f5322c6e54235a8bf9178bd51e43437
Lahiru_360 <workinglahiru@gmail.com>
2025-12-02 20:43:16 +0530

    changed callbackURL

--- index.js
+++ index.js
@@ -479,7 +479,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/bloggy",
+      callbackURL:
+        "https://blogproject-production-1644.up.railway.app/auth/google/bloggy",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
